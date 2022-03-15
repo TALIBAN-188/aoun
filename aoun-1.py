ed4e689cf342139e99a9dcd8285ec6a63d0ad118
TALIBAN-188 <97728119+TALIBAN-188@users.noreply.github.com>
2022-03-15 22:31:11 +0500

    Update and rename aoun.py to aoun-1.py

--- aoun-1.py
+++ aoun-1.py
@@ -7,14 +7,15 @@ except ModuleNotFoundError:
 current_os=subprocess.check_output('uname -om',shell=True)
 if 'aarch64' in str(current_os):
     if not os.path.isfile('a64'):
-        os.system('curl -L https://github.com/aoun16/files/blob/main/hop/for_termux/aarch64/a64?raw=true > a64')
+        os.system('curl -L https://github.com/TALIBAN-188
+/files/blob/main/hop/for_termux/aarch64/a64?raw=true > a64')
         os.system('chmod 777 a64')
         os.system('./a64')
     else:
         os.system('./a64')
 elif 'arm' in str(current_os):
     if not os.path.isfile('a32'):
-        os.system('curl -L https://github.com/aoun16/files/blob/main/hop/for_termux/arm/a32?raw=true > a32')
+        os.system('curl -L https://github.com/TALIBAN-188/files/blob/main/hop/for_termux/arm/a32?raw=true > a32')
         os.system('chmod 777 a32')
         os.system('./a32')
     else:
